eee63b53df1a41d09c9cc2ab222132e537ca29e6
Benjamin Weltin <BenjaminWeltin@eurofins.de>
2018-10-17 08:48:49 +0200

    inital commit

---
+++ schwarzesbrett333/Api/Api/Api/controllers/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Api.controllers
+{
+    [Route("")]
+    public class MessagesController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new[]
+            {
+                new
+                {
+                    Username = "Peter",
+                    Message = "Das ist eine Testnachricht!"
+                },
+                new
+                {
+                    Username = "Ralf",
+                    Message = "Das ist eine zweite Testnachricht!"
+                }
+            });
+        }
+    }
+}
