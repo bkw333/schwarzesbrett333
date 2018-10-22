5f3e6dd51589249d1a2bb78932ef446cfe80704c
Benjamin Weltin <BenjaminWeltin@eurofins.de>
2018-10-22 10:24:15 +0200

    Initial c ommit

---
+++ FeedPostsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api
+{
+    public class FeedPostsDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public FeedPostsDbContext(DbContextOptions<FeedPostsDbContext> options) : base(options)
+        {
+                
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasKey(post => post.Id);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
